d60bc77e221f1b0ea7bad255890b1ab890659740
yangpingyan <yangpingyan@gmail.com>
2017-07-15 15:17:55 +0800

    t

--- urllib.py
+++ urllib.py
@@ -6,33 +6,33 @@ Created on Tue Jun 27 09:13:38 2017
 @author: zhanghua
 """
 
-import urllib.request
-import http.cookiejar
-
-url = "http://www.baidu.com"
-
-print('第一种方法')
-response1 = urllib.request.urlopen(url)
-print(response1.getcode())
-print(len(response1.read()))
-print(response1.read())
-
-print('second method')
-request = urllib.request.Request(url)
-request.add_header("user-agent", "Mozilla/5.0")
-response2 = urllib.request.urlopen(request)
-print(response2.getcode())
-print(len(response2.read()))
-print(response2.read())
-
-
-print('Third mesthod')
-cj = http.cookiejar.CookieJar()
-opener = urllib.request.build_opener(urllib.request.HTTPCookieProcessor(cj))
-response3 = urllib.request.urlopen(url)
-print(response3.getcode())
-print(cj)
-print(response3.read())
+#import urllib.request
+#import http.cookiejar
+#
+#url = "http://www.baidu.com"
+#
+#print('第一种方法')
+#response1 = urllib.request.urlopen(url)
+#print(response1.getcode())
+#print(len(response1.read()))
+#print(response1.read())
+#
+#print('second method')
+#request = urllib.request.Request(url)
+#request.add_header("user-agent", "Mozilla/5.0")
+#response2 = urllib.request.urlopen(request)
+#print(response2.getcode())
+#print(len(response2.read()))
+#print(response2.read())
+#
+#
+#print('Third mesthod')
+#cj = http.cookiejar.CookieJar()
+#opener = urllib.request.build_opener(urllib.request.HTTPCookieProcessor(cj))
+#response3 = urllib.request.urlopen(url)
+#print(response3.getcode())
+#print(cj)
+#print(response3.read())
 
 
 
