bf36574ee80ea3a1493c35e13f06d8242a6e3c04
yangpingyan <yangpingyan@gmail.com>
2018-08-20 18:15:35 +0800

    1

--- datamining.py
+++ datamining.py
@@ -3,8 +3,6 @@
 # @Time : 2018/8/20 17:48 
 # @Author : yangpingyan@gmail.com
 
-# coding: utf-8
-
 # ML项目的完整流程
 # 1. 项目概述。
 # 2. 获取数据。
@@ -15,22 +13,19 @@
 # 7. 给出解决方案。
 # 8. 部署、监控、维护系统。
 
-# Common imports
 import csv
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
 from matplotlib.colors import ListedColormap
-from sklearn import metrics, pipeline
-from sklearn import feature_selection
-from sklearn import preprocessing, model_selection
-from sklearn import svm
-
 import time
 import os
 
-# to make this notebook's output stable across runs
+# to make this notebook's output identical at every run
 np.random.seed(42)
+
+# to make this notebook's output stable across runs
 pd.set_option('display.max_columns', 50)
 pd.set_option('display.max_rows', 10)
 plt.rcParams['axes.labelsize'] = 14
@@ -72,8 +67,6 @@ def fetch_housing_data(housing_url=HOUSING_URL, housing_path=HOUSING_PATH):
 
 
 # fetch_housing_data()
-
-
 def load_housing_data(housing_path=HOUSING_PATH):
     csv_path = os.path.join(housing_path, "housing.csv")
     return pd.read_csv(csv_path)
@@ -86,68 +79,22 @@ housing["ocean_proximity"].value_counts()
 housing.describe()
 housing.hist(bins=50, figsize=(20, 15))
 save_fig("attribute_histogram_plots")
-plt.show()
-
-# to make this notebook's output identical at every run
-np.random.seed(42)
-
-train_set, test_set = model_selection.train_test_split(housing, test_size=0.2, random_state=42)
-test_set.head()
-
-# 数据集中的每个分层都要有足够的实例位于你的数据中，这点很重要。否则，对分层重要性的评估就会有偏差。
-# 这意味着，你不能有过多的分层，且每个分层都要足够大
-housing["median_income"].hist()
-# Divide by 1.5 to limit the number of income categories
-housing["income_cat"] = np.ceil(housing["median_income"] / 1.5)
-# Label those above 5 as 5
-housing["income_cat"].where(housing["income_cat"] < 5, 5.0, inplace=True)
-housing["income_cat"].value_counts()
-housing["income_cat"].hist()
-
-split = model_selection.StratifiedShuffleSplit(n_splits=1, test_size=0.2, random_state=42)
-for train_index, test_index in split.split(housing, housing["income_cat"]):
-    strat_train_set = housing.loc[train_index]
-    strat_test_set = housing.loc[test_index]
-
-strat_test_set["income_cat"].value_counts() / len(strat_test_set)
-housing["income_cat"].value_counts() / len(housing)
-
-
-def income_cat_proportions(data):
-    return data["income_cat"].value_counts() / len(data)
-
-
-train_set, test_set = model_selection.train_test_split(housing, test_size=0.2, random_state=42)
-
-compare_props = pd.DataFrame({
-    "Overall": income_cat_proportions(housing),
-    "Stratified": income_cat_proportions(strat_test_set),
-    "Random": income_cat_proportions(test_set),
-}).sort_index()
-compare_props["Rand. %error"] = 100 * compare_props["Random"] / compare_props["Overall"] - 100
-compare_props["Strat. %error"] = 100 * compare_props["Stratified"] / compare_props["Overall"] - 100
-
-compare_props
-
-for set_ in (strat_train_set, strat_test_set):
-    set_.drop("income_cat", axis=1, inplace=True)
+# plt.show()
 
 # # Discover and visualize the data to gain insights
-housing = strat_train_set.copy()
 housing.plot(kind="scatter", x="longitude", y="latitude")
-save_fig("bad_visualization_plot")
+# save_fig("bad_visualization_plot")
 
 housing.plot(kind="scatter", x="longitude", y="latitude", alpha=0.1)
-save_fig("better_visualization_plot")
+# save_fig("better_visualization_plot")
 
 housing.plot(kind="scatter", x="longitude", y="latitude", alpha=0.4,
              s=housing["population"] / 100, label="population", figsize=(10, 7),
              c="median_house_value", cmap=plt.get_cmap("jet"), colorbar=True,
              sharex=False)
 plt.legend()
-save_fig("housing_prices_scatterplot")
+# save_fig("housing_prices_scatterplot")
 
-import matplotlib.image as mpimg
 
 california_img = mpimg.imread(PROJECT_ROOT_DIR + '/images/end_to_end_project/california.png')
 ax = housing.plot(kind="scatter", x="longitude", y="latitude", figsize=(10, 7),
@@ -167,7 +114,7 @@ cbar.ax.set_yticklabels(["$%dk" % (round(v / 1000)) for v in tick_values], fonts
 cbar.set_label('Median House Value', fontsize=16)
 
 plt.legend(fontsize=16)
-save_fig("california_housing_prices_plot")
+# save_fig("california_housing_prices_plot")
 plt.show()
 
 corr_matrix = housing.corr()
@@ -178,11 +125,11 @@ from pandas.plotting import scatter_matrix
 attributes = ["median_house_value", "median_income", "total_rooms",
               "housing_median_age"]
 scatter_matrix(housing[attributes], figsize=(12, 8))
-save_fig("scatter_matrix_plot")
+# save_fig("scatter_matrix_plot")
 
 housing.plot(kind="scatter", x="median_income", y="median_house_value", alpha=0.1)
 plt.axis([0, 16, 0, 550000])
-save_fig("income_vs_house_value_scatterplot")
+# save_fig("income_vs_house_value_scatterplot")
 
 housing["rooms_per_household"] = housing["total_rooms"] / housing["households"]
 housing["bedrooms_per_room"] = housing["total_bedrooms"] / housing["total_rooms"]
@@ -197,3 +144,6 @@ plt.axis([0, 5, 0, 520000])
 plt.show()
 
 housing.describe()
+
+
+print("Missiong Complete!")
