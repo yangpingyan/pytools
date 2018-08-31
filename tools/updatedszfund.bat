b56c3d5d82cbae27a7ddb96bfff25c87afdaff7e
yangpingyan <yangpingyan@gmail.com>
2018-08-31 10:15:42 +0800

    1

---
+++ tools/updatedszfund.bat
@@ -0,0 +1,7 @@
+set /p var=请输入总资产:
+cd C:\Users\yangp\iCloudDrive\work\pytools
+C:\Users\yangp\Anaconda3\python C:\Users\yangp\iCloudDrive\work\pytools\qingtianzhufund.py --assets %var%
+pause
+
+
+
